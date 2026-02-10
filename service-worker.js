f05ac708142d69756218b1a893ecfc1149f77180
salome_shion_winbr <60033251+window794@users.noreply.github.com>
2026-02-11 02:31:26 +0900

    Update service-worker.js
    Fix PWA paths for GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,12 @@
 
 const CACHE_NAME = 'microwave-calc-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // インストール時にキャッシュを作成
